c89cd8f9f8f0d0ed88d4c54bac2f9e7707a12899
Kevin Mahoney <kjmahoney13@gmail.com>
2016-12-22 19:43:19 -0500

    added regexp to split multi syllable words

--- app.js
+++ app.js
@@ -7,43 +7,51 @@ var pinyinVowelsArray = {
   v: ["ǖ", "ǘ", "ǚ", "ǜ"]
 }
 
-
 function makeArray(word){
   //break string into an array and reverse
- let wordArray = word.split("").reverse()
- //determine the tone of the word
+  let wordArray = word.split("").reverse()
 
-
-//Determine which letter should have the tone according to pinyin rules
- for (i=0; i< wordArray.length; i++){
-  //  A and e trump all other vowels and always take the tone mark.
-   if (wordArray[i] == "a" || wordArray[i] == "e"){
-     targetVowel = wordArray[i]
-     break
-   }
-   // In the combination ou, o takes the mark.
+  //can probably do a case here
+  //Determine which letter should have the tone according to pinyin rules
+  for (i=0; i< wordArray.length; i++){
+    //  a and e trump all other vowels and always take the tone mark.
+    if (wordArray[i] == "a" || wordArray[i] == "e"){
+      targetVowel = wordArray[i]
+      break
+    }
+    // In the combination ou, o takes the mark.
     else if(wordArray[i]=="o" && wordArray[i+1] == "u"){
       targetVowel = wordArray[i]
       break
     }
     // In all other cases, the final vowel takes the mark.
     else if (wordArray[i] == "o" || wordArray[i] == "i" || wordArray[i] == "u"){
-     targetVowel = wordArray[i]
-   }
- }
- getPinyin(targetVowel, wordArray)
- }
+      targetVowel = wordArray[i]
+    }
+  }
+  getPinyin(targetVowel, wordArray)
+}
 
 function getPinyin(targetVowel, wordArray){
+  //determine the tone of the word
   let toneNumber = wordArray[0]
-  pinyinVowel = pinyinVowelsArray[targetVowel][toneNumber -1]
+  let pinyinVowel = pinyinVowelsArray[targetVowel][toneNumber -1]
   wordArray.splice(wordArray.indexOf(targetVowel),1,pinyinVowel)
   wordArray.splice(0,1)
   let newWord = wordArray.reverse().join("")
   console.log(newWord)
 }
 
- //find the proper tone in the object
+function chop(words){
+  wordsNew = words.split(/(?=[\s=\d])/);
+  console.log(wordsNew)
+}
+// /(?=[\s=)(])/
+
+var string = "this is a string,and I have a comma";
+var array = string.split(/[1234,]+/);
+
+//find the proper tone in the object
 
 // function getTone(wordArray){
 //   let toneNumber = wordArray[0]
